81e3d3c7b88c34913dee0a75dd14d3a24d5d6bea
Tobias Hahnen <hahnen.tobi@gmail.com>
2022-04-16 15:10:45 +0200

    Added SonarQube integration via SonarCloud!

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.4.20"
     id("com.gradle.plugin-publish") version "0.14.0"
     id("io.gitlab.arturbosch.detekt") version "1.19.0-RC2"
+    id("org.sonarqube") version "3.3"
 }
 
 
